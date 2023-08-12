a5cddb05cdb3b56b14cb1ffccf4d660887474e95
Nataniel Malysh <114068925+Natan2511@users.noreply.github.com>
2023-08-12 16:41:06 +0300

    project complete

---
+++ src/script/swiper.js
@@ -0,0 +1,10 @@
+import Swiper from 'swiper';
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    el: '.swiper-pagination',
+    el: '.swiper-pagination',
+  },
+});
